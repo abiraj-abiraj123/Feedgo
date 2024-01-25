7025f0a73b8fdef7fa7fab498ae8a02d88878746
abiraj-abiraj123 <abirajcs161@gmail.com>
2024-01-25 09:58:06 +0530

    New One

--- src/Components/Login/Loginpage.jsx
+++ src/Components/Login/Loginpage.jsx
@@ -10,20 +10,20 @@ import './Login.css';
 import Validation from './LoginValidate';
 
  const Loginpage = () => {
-    const [values,setValue]=useState({
-        email:'',
-        password:''
-    })
-    const [errors,setErrors]=useState({
+    // const [values,setValue]=useState({
+    //     email:'',
+    //     password:''
+    // })
+    // const [errors,setErrors]=useState({
 
-    })
-    const handleInput=(event)=>{
-        setValues(prev=>({...prev,[event.target.name]:[event.target.value]}))
-    }
-    const handleSubmit=(event)=>{
-        event.preventDefault();
-        setError(Validation(values));
-    }
+    // })
+    // const handleInput=(event)=>{
+    //     setValues(prev=>({...prev,[event.target.name]:[event.target.value]}))
+    // }
+    // const handleSubmit=(event)=>{
+    //     event.preventDefault();
+    //     setError(Validation(values));
+    // }
     const navigate = useNavigate();
     return (
         <div>
@@ -45,27 +45,34 @@ import Validation from './LoginValidate';
                                 <Form.Group as={Row} className='mb-3' controlId='formplaintextId'>
                                     <Form.Label column sm="2">
                                         <div className='text1' name="userid"
-                                        onChange={handleInput}><strong>E-mail:</strong></div>
+                                        // onChange={handleInput}
+                                        ><strong>E-mail:</strong></div>
                                     </Form.Label>
                                     <Col sm='5'>
                                         <div className='input1'><Form.Control type="text" placeholder='email' /></div>
-                                        {errors.email && <span className='text-danger'>{errors.email}</span>}
+                                        {/* {errors.email && <span className='text-danger'> */}
+                                            {/* {errors.email} */}
+                                            {/* </span> */}
+                                            {/* } */}
                                     </Col>
                                 </Form.Group>
 
                                 <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                                     <Form.Label column sm="2">
                                         <div className="text1" name="password"
-                                        onChange={handleInput}><strong>Password:</strong></div>
+                                        // onChange={handleInput}
+                                        >
+                                            <strong>Password:</strong></div>
                                     </Form.Label>
                                     <Col sm="5" required>
                                         <div className="input1"><Form.Control type="text" placeholder="Password" /></div>
-                                        {errors.password && <span className='text-danger'>{errors.password}</span>}
+                                        {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
                                     </Col>
                                 </Form.Group>
 
                                 <div className='btn1'><button type="submit" className='mb-4 px-5' color="dark" size='sm' onClick={() => navigate("card")}
-                                onSubmit={handleSubmit}>Login</button></div>
+                                // onSubmit={handleSubmit}
+                                >Login</button></div>
                                 <div className='a11'><a className='small text-muted' href="#">Forgot Password?</a></div>
                                 <h6 className='mb-5 pb-lg-2' style={{ color: '#393f81' }} >Not a user?<a href="#" style={{ color: '#393f81' }} onClick={() => navigate('signup')}>SignUp!!!</a></h6>
 
