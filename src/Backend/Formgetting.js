8cc41ae46d25ae2d6a2b47a95adbc3441b7a31af
S9963hd <sanjaymajestic143@gmail.com>
2024-02-23 09:40:56 +0530

    Backend

--- src/Backend/Formgetting.js
+++ src/Backend/Formgetting.js
@@ -1,20 +1,20 @@
-let express=require('express');
-let app=express();
-let Model=require('./Mongodb.js');
-app.post('/question',(req,res)=>{
-    let data=req.body;
-    let object=new Model();
-    object.save(data);
-})
-app.get('/question',(req,res)=>{
-    let data=req.query;
-    let object=new Model();
-    res=object.findAll(data);
+// let express=require('express');
+// let app=express();
+// let Model=require('./Mongodb.js');
+// app.post('/question',(req,res)=>{
+//     let data=req.body;
+//     let object=new Model();
+//     object.save(data);
+// })
+// app.get('/question',(req,res)=>{
+//     let data=req.query;
+//     let object=new Model();
+//     res=object.findAll(data);
 
-})
-app.update('/question',(req,res)=>{
-    let data=req.query;
-    let object=new Model();
-    res=object.findAll(data);
+// })
+// app.update('/question',(req,res)=>{
+//     let data=req.query;
+//     let object=new Model();
+//     res=object.findAll(data);
 
-})
+// })
--- src/Backend/Mongodb.js
+++ src/Backend/Mongodb.js
@@ -1,9 +1,9 @@
-require('mongoose');
-let mongoUrl="";
-export let mongodb=mongoose.connect("").then(res=>console.log("Connection Establish")).catch(err=>console.log("Something Wrong"));
+// require('mongoose');
+// let mongoUrl="";
+// export let mongodb=mongoose.connect("").then(res=>console.log("Connection Establish")).catch(err=>console.log("Something Wrong"));
 
-let Schema=new mongoose.Schema({
-    question:{type:string,required},
-    options:{type:string,required},
-})
-let model=mongoose.model("questions",Schema);
+// let Schema=new mongoose.Schema({
+//     question:{type:string,required},
+//     options:{type:string,required},
+// })
+// let model=mongoose.model("questions",Schema);
